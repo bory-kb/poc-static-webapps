840d4c4e2a2e187b4b7916df56781905eb147a79
bory-kb <s.kubo0527@gmail.com>
2021-09-08 20:08:19 +0900

    add-api

---
+++ api/HttpTrigger1.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class HttpTrigger1
+    {
+        [FunctionName("message")]
+        public static async Task<string> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            log.LogInformation(responseMessage);
+            var retData = JsonConvert.SerializeObject(responseMessage);
+            return retData;
+        }
+    }
+}
